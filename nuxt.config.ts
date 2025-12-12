24dea6659512fdffc974a8c4d742f86574f92ef8
tops-jayendrasinh <jayendrasinh@topsinfosolutions.com>
2025-12-12 11:56:59 +0530

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
-import { config } from './layers/01.base/config/base'
-
-console.log("config :", config)
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // NOTE: This doesn't work as you can check auth middleware runs before base middleware
+  extends: [
+    './layers/base/',
+    './layers/auth/',
+  ],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
