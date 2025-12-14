b4382b884d4fbae83ee5ffc6fc8255589963aa46
JD Solanki <jdsolanki0001@gmail.com>
2025-12-14 14:28:22 +0530

    init

---
+++ app/app.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const onChange = () => {
+  updateAppConfig({
+    ui: {
+      button: {
+        variants: {
+          size: {
+            md: 'py-2 px-4'
+          }
+        }
+      }
+    }
+  })
+}
+</script>
+
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <UButton>Button</UButton>
+    <UButton @click="onChange">Change</UButton>
+  </div>
+</template>
