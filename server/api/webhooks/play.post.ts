3bd715e05b2ace8a909aa8a33db7b83599f54a44
JD Solanki <jdsolanki0001@gmail.com>
2026-01-23 15:32:23 +0530

    use readRawBody

--- server/api/webhooks/play.post.ts
+++ server/api/webhooks/play.post.ts
@@ -1,8 +1,8 @@
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
+  const body = await readRawBody(event)
   console.log('Received webhook payload:', body)
   
-  const body2 = await readBody(event)
+  const body2 = await readRawBody(event)
   console.log('Received webhook payload (2nd read):', body2)
     
   return { body }
